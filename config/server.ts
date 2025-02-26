4c27d576e9da24dbe46bf16c8f31faa9e49c5dca
f.staikos7 <f.staikos7@gmail.com>
2025-02-26 19:36:21 +0200

    1

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,13 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1338),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-});
+export default ({ env }) => {
+  // Log APP_KEYS to check if it's correctly read
+  console.log('APP_KEYS:', env('APP_KEYS'));
+  console.log('APP_KEYS as array:', env.array('APP_KEYS'));
+
+  return {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1338),
+    app: {
+      keys: env.array('APP_KEYS'),
+    },
+  };
+};
