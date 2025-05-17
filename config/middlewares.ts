a908a5132cc14d897283aaa59a8ab6f24fd76e06
f.staikos7 <f.staikos7@gmail.com>
2025-05-17 11:42:25 +0300

    a

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,34 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  // Remove duplicate 'strapi::security' if present above!
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "script-src": ["'self'", "'unsafe-inline'", "https:", "blob:", "editor.unlayer.com"],
+          "script-src-elem": ["'self'", "'unsafe-inline'", "https:", "blob:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://res.cloudinary.com", // <-- Add this line!
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://res.cloudinary.com", // <-- Add this line!
+          ],
+          "connect-src": ["'self'", "http:", "https:"],
+          "frame-src": ["'self'", "editor.unlayer.com"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
@@ -9,21 +36,4 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "script-src": ["'self'", "'unsafe-inline'", "https:", "blob:","editor.unlayer.com"],
-          "script-src-elem": ["'self'", "'unsafe-inline'", "https:", "blob:"],
-          "img-src": ["'self'", "data:", "blob:"],
-          "media-src": ["'self'", "data:", "blob:"],
-          "connect-src": ["'self'", "http:", "https:"], // Allow HTTP & HTTPS
-          "frame-src": ["'self'", "editor.unlayer.com"],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
 ];
